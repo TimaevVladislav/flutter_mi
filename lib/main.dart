81c2dd63d4f6fdaa4690441528551d54c7d16177
timaevV <timaevvladislav@gmail.com>
2023-07-18 08:05:53 +0600

    add:component widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(),
+      ),
+    );
+  }
 }
