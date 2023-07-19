c7d7328fdec7b4355edf0d05079a58e6857d6a3d
timaevV <timaevvladislav@gmail.com>
2023-07-20 01:41:46 +0600

    add:completed chellange

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,39 @@ class Application extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.red,
-                child: Text("Application"),
+              Column(
+                children: <Widget>[
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red),
+                  Container(height: 100.0, width: 100.0, color: Colors.red)
+                ],
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text("Application"),
+              Row(
+                children: [
+                  Container(height: 100.0, width: 100.0, color: Colors.yellow),
+                  Container(height: 100.0, width: 100.0, color: Colors.green)
+                ],
               ),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.yellow,
-                child: Text("Application"),
-              )
+              Column(
+                children: <Widget>[
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue),
+                  Container(height: 100.0, width: 100.0, color: Colors.blue)
+                ],
+              ),
             ],
           ),
         ),
