25da05a9616e4707f8b65a312d34082b811f7848
timaevV <timaevvladislav@gmail.com>
2023-07-21 00:25:38 +0600

    add:form template

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,49 @@ class Application extends StatelessWidget {
                       color: Colors.teal.shade100,
                       fontWeight: FontWeight.bold,
                       fontSize: 20.0,
-                      letterSpacing: 2.5))
+                      letterSpacing: 2.5)),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text("+996 888 555 344",
+                        style: TextStyle(
+                            fontFamily: 'Italic',
+                            color: Colors.teal.shade900,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            letterSpacing: 2.5)),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text("timaevvladislav@gmail.com",
+                        style: TextStyle(
+                            fontFamily: 'Italic',
+                            color: Colors.teal.shade900,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            letterSpacing: 2.5)),
+                  ],
+                ),
+              )
             ],
           ),
         ),
